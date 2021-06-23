4c3e774b67f63f4fe0875cda6fb1d74d5a33ac5a
leosar <ldosto>
2021-06-23 23:25:24 +0200

    Initial commit

---
+++ src/main/java/com/leo/test/leoubstest/LeoUbsTestApplication.java
@@ -0,0 +1,13 @@
+package com.leo.test.leoubstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoUbsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoUbsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/test/leoubstest/LeoUbsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.test.leoubstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoUbsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
